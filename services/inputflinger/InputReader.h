f2c2014c2a15c7fd8b0dc8e99659516d615b08de
Steve Kondik <shade@chemlab.org>
2015-02-01 23:40:19 -0500

    input: Add option to toggle pointer icon when using stylus(3/3)
    * The visible pointer icon when hovering or drawing with the stylus is
      quite annoying when trying to actually draw with it. Turn it off by
      default and add an option to turn it on.
    
    Forward Port from CM-11.0
    
    Change-Id: Ie4e9e6bcc48803b195d1615d83d6e36d663cc33a

--- services/inputflinger/InputReader.h
+++ services/inputflinger/InputReader.h
@@ -144,13 +144,14 @@ struct InputReaderConfiguration {
         // The presence of an external stylus has changed.
         CHANGE_EXTERNAL_STYLUS_PRESENCE = 1 << 7,
 
-<<<<<<< refs/remotes/aospa/nougat-mr2
         // Swap keys changed.
         CHANGE_SWAP_KEYS = 1 << 8,
-=======
+
         // Volume keys rotation option changed.
-        CHANGE_VOLUME_KEYS_ROTATION = 1 << 7,
->>>>>>> HEAD~68
+        CHANGE_VOLUME_KEYS_ROTATION = 1 << 9,
+
+        // Stylus icon option changed.
+        CHANGE_STYLUS_ICON_ENABLED = 1 << 10,
 
         // All devices must be reopened.
         CHANGE_MUST_REOPEN = 1 << 31,
@@ -239,15 +240,18 @@ struct InputReaderConfiguration {
     // True to show the location of touches on the touch screen as spots.
     bool showTouches;
 
-    // Remap volume keys according to display rotation
-    // 0 - disabled, 1 - phone or hybrid rotation mode, 2 - tablet rotation mode
-    int volumeKeysRotationMode;
+    // True to show the pointer icon when a stylus is used.
+    bool stylusIconEnabled;
 
     // Ignore finger touches this long after the stylus has been used (including hover)
     nsecs_t stylusPalmRejectionTime;
 
     // Swap back with recents button
     bool swapKeys;
+
+    // Remap volume keys according to display rotation
+    // 0 - disabled, 1 - phone or hybrid rotation mode, 2 - tablet rotation mode
+    int volumeKeysRotationMode;
 
     InputReaderConfiguration() :
             virtualKeyQuietTime(0),
@@ -265,14 +269,12 @@ struct InputReaderConfiguration {
             pointerGestureSwipeMaxWidthRatio(0.25f),
             pointerGestureMovementSpeedRatio(0.8f),
             pointerGestureZoomSpeedRatio(0.3f),
-            stylusPalmRejectionTime(50 * 10000000LL), // 50 ms
             showTouches(false),
-<<<<<<< refs/remotes/aospa/nougat-mr2
             swapKeys(false) { }
-=======
-            volumeKeysRotationMode(0) { }
->>>>>>> HEAD~68
-
+            stylusIconEnabled(false),
+            stylusPalmRejectionTime(50 * 10000000LL), // 50 ms
+            volumeKeysRotationMode(0)
+    { }
     bool getDisplayInfo(bool external, DisplayViewport* outViewport) const;
     void setDisplayInfo(bool external, const DisplayViewport& viewport);
 
@@ -1916,6 +1918,8 @@ private:
 
     static void assignPointerIds(const RawState* last, RawState* current);
     
+    void unfadePointer(PointerControllerInterface::Transition transition);
+
     bool rejectPalm(nsecs_t when);
 };
 
